429777887514d168ab29b858b0f4474805620994
Chalindu Nisal <chaindunisal1998@gmail.com>
2024-08-12 22:09:21 +0530

    Create the database and collections

--- Back-end/index.js
+++ Back-end/index.js
@@ -40,6 +40,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //create the database and collection
+    const database = client.db("online-learning");
+    const userCollection = database.collection("users");
+    const classesCollection = database.collection("classes");
+    const cartCollection = database.collection("cart");
+    const paymentCollection = database.collection("payments");
+    const enrolledCollection = database.collection("enrolled");
+    const appliedCollection = database.collection("applied");
+
+    //classes route here
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
