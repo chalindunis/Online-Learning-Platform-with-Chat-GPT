e2bcc7b1ea359f526aa3d233d6f2b337184c1300
Chalindu Nisal <chaindunisal1998@gmail.com>
2024-08-14 12:13:05 +0530

    Classes routes added

--- Back-end/index.js
+++ Back-end/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 
 // Mondodb connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-online-learnin.uybyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-online-learning`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -70,19 +70,92 @@ async function run() {
     //Delete a user
     //Update a user
 
-    //classes route here
-    //get data from the database showing approved classes
-    //Get all classes added  by instructor (instructor email)
-    
+    //---------CLASSES ROUTE-----------
 
-    //manage classes
-    
+    //Add new classes
+    app.post('/new-class',  async (req, res) => {
+      const newClass = req.body;
+      newClass.availableSeats = parseInt(newClass.availableSeats)
+      const result = await classesCollection.insertOne(newClass);
+      res.send(result);
+  });
+
+    //Get approved classes from the database
+    app.get('/classes', async (req, res) => {
+      const query = { status: 'approved' };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+  })
+
+
+    //Get all classes added  by instructor (instructor email)
+    app.get('/classes/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { instructorEmail: email};
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+  })
+
+    //Manage classes
+    app.get('/classes-manage', async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+  })
+
+    //Update class status and reason
+    app.patch('/change-status/:id', async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const reason =req.body.reason;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updateDoc = {
+        $set: {
+          status:status,
+          reason: reason,
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+  })
+
+
     //Get all classes
     //Change status of a class
-    //get approved classes
+    //Get approved classes
+    app.get('/approved-classes', async (req, res) => {
+      const query = { status: 'approved' };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+  })
+
     //Get all instructors
-    //Update a class
+    //Update a class details
+    app.put('/update-class/:id', async (req, res) => {
+      const id = req.params.params.id;
+      const updateClass = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updateDoc = {
+        $set: {
+          name: updateClass.name,
+          description: updateClass.description,
+          availableSeats: parseInt(updateClass.availableSeats),
+          videoLink: updateClass.videoLink,
+          status:'pending',
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+  })
+
     //get single class by id for details page
+    app.get('class/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)};
+      const result = await classesCollection.findOne(query);
+      res.send(result);
+  })
 
     //add to cart
     //get cart item id for checking if a class is already in cart
